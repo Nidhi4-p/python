abeb113d844a512e396e2757463f79b910b17679
Nidhi4-p <31071328+Nidhi4-p@users.noreply.github.com>
2018-10-28 21:32:13 +0530

    Update and rename binary.py to p5.py

---
+++ python practice/p5.py
@@ -0,0 +1,15 @@
+'''question 5:
+Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+Example:
+0100,0011,1010,1001
+Then the output should be:
+1010'''
+
+s=[]
+n=input("enter the 4 digit binary numbers seperated by commas:\n").split(',')
+for i in n:
+	x=int(i,2)
+	if (x%5==0):
+		s.append(i)	
+print(",".join(s))	
+
