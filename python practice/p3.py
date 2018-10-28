8b583e9ff41e8399c670d432f85f242fe0df17e6
Nidhi4-p <31071328+Nidhi4-p@users.noreply.github.com>
2018-10-28 12:48:58 +0530

    3 questions with solution

--- python practice/p3.py
+++ python practice/p3.py
@@ -4,7 +4,9 @@ Suppose the following input is supplied to the program:
 hello world and practice makes perfect and hello world again
 Then, the output should be:
 again and hello makes perfect practice world'''
+
 #code:
+
 s=[]
 a=input("enter the words:\n").split(" ")
 a=set(a)
