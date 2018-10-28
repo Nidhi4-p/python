398d20d13fb4508fe9bcac9d701fa9ab19ed348a
Nidhi4-p <31071328+Nidhi4-p@users.noreply.github.com>
2018-10-28 12:51:23 +0530

    3 questions with solution
    questions are attached in the program file.

--- python practice/bank.py
+++ python practice/bank.py
@@ -1,4 +1,4 @@
-'''question:
+'''question1 :
 Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following:
 D 100
 W 200
