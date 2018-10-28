8c704448efd6d2a06d8f9f749eb12ba3f6db7e27
Nidhi4-p <31071328+Nidhi4-p@users.noreply.github.com>
2018-10-28 12:52:53 +0530

    Update p3.py

--- python practice/p3.py
+++ python practice/p3.py
@@ -1,4 +1,4 @@
-'''question:
+'''question3:
 Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically.
 Suppose the following input is supplied to the program:
 hello world and practice makes perfect and hello world again
