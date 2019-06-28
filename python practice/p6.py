c74b6aa063fd448c4c959078e7cbd68b312745d3
Nidhi4-p <31071328+Nidhi4-p@users.noreply.github.com>
2019-06-28 10:15:35 +0530

    Create p6.py

---
+++ python practice/p6.py
@@ -0,0 +1,50 @@
+"""Given an array A[] of size N and an integer K. Your task is to complete the function countDistinct() which prints the count of distinct numbers in all windows of size k in the array A[].
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains two integers N and K. Then in the next line are N space separated values of the array A[].
+
+Output:
+For each test case in a new line print the space separated values denoting counts of distinct numbers in all windows of size k in the array A[].
+
+Constraints:
+1 <= T <= 100
+1 <= N, K <= 100
+1 <= A[] <= 100
+
+Example(To be used only for expected output):
+Input:
+2
+7 4
+1 2 1 3 4 2 3
+3 2
+4 1 1
+
+Output:
+3 4 4 3
+2 1"""
+
+
+
+def countDistinct(arr, n, k):
+	a=[]
+	g=[]
+	t=[]
+	m=r=0
+	while(r<n):
+		r=m+k
+		m+=1
+	for i in range(0,m):
+		for j in range(i,k+i):
+			a.append(arr[j])	
+		for o in a:
+			if o not in g:
+				g.append(o)
+			l=len(g)
+		a.clear()
+		g.clear()
+		t.append(l)
+	print(" ".join(map(str,list(t))))
+u=int(input())
+n,k=map(int,input().split())
+arr=input().split()
+countDistinct(arr, n, k)
